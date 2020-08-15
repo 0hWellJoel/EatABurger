70b14439d21fe79b15131de5ae175ab74860cfb9
0hWellJoel <61248738+0hWellJoel@users.noreply.github.com>
2020-08-15 14:08:36 -0400

    updates

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+### Schema
+
+CREATE DATABASE burger_db;
+USE burger_db;
+
+
+create table burgers(
+    id int primary key not null auto_increment,
+    burger_name varchar(30),
+    devoured boolean
+);
